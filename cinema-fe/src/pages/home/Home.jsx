ab2cbc9bcd34794bf7133ff391a245cca44da8f7
Vinh Ngô Quang <114270673+VinhNgo2001@users.noreply.github.com>
2022-12-07 15:57:20 +0700

    pages home

--- cinema-fe/src/pages/home/Home.jsx
+++ cinema-fe/src/pages/home/Home.jsx
@@ -1,4 +1,3 @@
-import Carousel from 'react-bootstrap/Carousel';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,12 +6,10 @@ import Card from 'react-bootstrap/Card';
 import React from "react";
 import Slider from "react-slick";
 
-import carousel from '../../img/980wx448h_18.jpg'
-import carousel1 from '../../img/cgv_980x488.png'
-import carousel2 from '../../img/banner_980x448.jpg'
-import poster from '../../img/poster_adam_4_1.jpg'
+
 
 import Slick from './slick/Slick'
+import Carousels from './carousel/Carousel'
 import './home.css'
 
 import { useEffect, memo } from 'react'
@@ -24,45 +21,7 @@ const Home = memo(() => {
     return (
       <Container>
         <Row>
-          <Carousel>
-          <Carousel.Item>
-            <img
-            className="d-block w-100"
-            src={carousel }
-            alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={carousel1 }
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={carousel2 }
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          </Carousel>
+        <Carousels/>
         </Row>
         <Row>
           <div className="home-tile">
@@ -80,9 +39,6 @@ const Home = memo(() => {
         <Row>
           <Slick/>
         </Row> 
-        
-      
-        
       </Container>
     
     )
