016f0c426e82e6601d7ebb3114120d2194623f5c
Tran Cuong <tranquocc1@gmail.com>
2022-12-09 00:21:15 +0700

    Add backend and login

--- cinema-fe/src/main.jsx
+++ cinema-fe/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store} >
     <BrowserRouter>
       <Routes>
         <Route path='/*' element={<App />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 )
--- cinema-fe/src/pages/home/Home.jsx
+++ cinema-fe/src/pages/home/Home.jsx
@@ -15,46 +15,46 @@ import { useEffect, memo } from 'react'
 const Home = memo(() => {
 
 
-    useEffect(() => {
-    }, [])
-    return (
-      <Container>
-        <Row>
-        <Carousels/>
-        </Row>
-        <Row>
-          <div className="home-tile">
-            <h2>Movie selection</h2>
-          </div>
-        </Row>
-        <Row>
-          <SlickMovie/>
-        </Row>
-        <Row>
-          <div className="home-tile">
-            <h2>Event</h2>
-          </div>
-        </Row>
-        <Row>
-          <SlickEvent/>
-        </Row>
-        <Row>
-          <div className="home-tile">
-            <br/>
-          </div>
-        </Row>
-        <Row xs={1} md={2} className="g-4">
-          {Array.from({ length: 2 }).map((_, idx) => (
-            <Col>
-              <Card>
-                <Card.Img variant="top" src={homePoster} />
-              </Card>
-            </Col>
-          ))}
+  useEffect(() => {
+  }, [])
+  return (
+    <Container>
+      <Row>
+        <Carousels />
       </Row>
-      </Container>
-    
-    )
+      <Row>
+        <div className="home-tile">
+          <h2>Movie selection</h2>
+        </div>
+      </Row>
+      <Row>
+        <SlickMovie />
+      </Row>
+      <Row>
+        <div className="home-tile">
+          <h2>Event</h2>
+        </div>
+      </Row>
+      <Row>
+        <SlickEvent />
+      </Row>
+      <Row>
+        <div className="home-tile">
+          <br />
+        </div>
+      </Row>
+      <Row xs={1} md={2} className="g-4">
+        {Array.from({ length: 2 }).map((_, idx) => (
+          <Col key={idx}>
+            <Card>
+              <Card.Img variant="top" src={homePoster} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+
+  )
 })
 
 export default Home
