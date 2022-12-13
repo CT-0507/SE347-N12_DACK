ef027d38a8b8a74cb104c43779bab2ab60a0d6c5
Vinh Ngô Quang <114270673+VinhNgo2001@users.noreply.github.com>
2022-12-13 14:11:50 +0700

    Update Home.jsx

--- cinema-fe/src/pages/home/Home.jsx
+++ cinema-fe/src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import React from "react";
+import Button from 'react-bootstrap/esm/Button';
+
 
 import SlickMovie from './slicks/SlickMovie'
 import SlickEvent from './slicks/SlickEvent'
@@ -18,41 +20,45 @@ const Home = memo(() => {
   useEffect(() => {
   }, [])
   return (
-    <Container>
-      <Row>
-        <Carousels />
-      </Row>
-      <Row>
-        <div className="home-tile">
-          <h2>Movie selection</h2>
-        </div>
-      </Row>
-      <Row>
-        <SlickMovie />
-      </Row>
-      <Row>
-        <div className="home-tile">
-          <h2>Event</h2>
-        </div>
-      </Row>
-      <Row>
-        <SlickEvent />
-      </Row>
-      <Row>
-        <div className="home-tile">
-          <br />
-        </div>
-      </Row>
-      <Row xs={1} md={2} className="g-4">
-        {Array.from({ length: 2 }).map((_, idx) => (
-          <Col key={idx}>
-            <Card>
-              <Card.Img variant="top" src={homePoster} />
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+ 
+      // <div id ={isClick ?'NoContainer':'container'}>
+        <Container className="py-4">
+          <Row className="my-2">
+            <Carousels />
+          </Row>
+          <Row className="my-2">
+            <div className="home-tile">
+              <h2 >MOVIE SELECTION</h2>
+              
+            </div>
+          </Row>
+          <Row className="my-2">
+            <SlickMovie />
+          </Row>
+          <Row className="my-2">
+            <div className="home-tile">
+              <h2>EVENT</h2>
+            </div>
+          </Row>
+          <Row className="my-2">
+            <SlickEvent />
+          </Row>
+          <Row className="my-2">
+            <div className="home-tile">
+              <br />
+            </div>
+          </Row>
+          <Row xs={1} md={2} className="g-4 my-2 ">
+            {Array.from({ length: 2 }).map((_, idx) => (
+              <Col key={idx}>
+                <Card>
+                  <Card.Img variant="top" src={homePoster}  />
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      // </div>
 
   )
 })
