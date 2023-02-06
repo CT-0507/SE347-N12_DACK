bfb8d8d6b2bbadb78ae5d7c680cf9896a1c5d942
levanphucdev <phuclevan497@gmail.com>
2023-02-06 14:53:25 +0700

    add history user

--- cinema-fe/src/pages/account/user/Layout.jsx
+++ cinema-fe/src/pages/account/user/Layout.jsx
@@ -17,45 +17,46 @@ import { useSignUpMutation } from "../authApiSlice";
 import { Outlet } from "react-router-dom";
 
 const Layout = memo(() => {
-    return (
-        <div className="layout-container">
-            <div className="col-left">
-                <div className="title">TÀI KHOẢN CGV</div>
-                <Navbar bg="light" variant="light">
-                <Nav defaultActiveKey="#" className="flex-column">
-                    <Nav.Link as={Link} to="/user">
-                        THÔNG TIN CHUNG
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="edit">
-                        CHI TIẾT TÀI KHOẢN
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="card">
-                        THẺ THÀNH VIÊN
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="point">
-                        ĐIỂM THƯỞNG
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="gift">
-                        THẺ QUÀ TẶNG
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="voucher">
-                        VOUCHER
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="coupon">
-                        COUPON
-                    </Nav.Link>
-                    <Nav.Link >LỊCH SỬ GIAO DỊCH</Nav.Link>
-                </Nav>
-                </Navbar>
-            </div>
-            <div className="col-right">
-                <div className="heading">TÀI KHOẢN CGV</div>
-                <div className="main">
-                    <Outlet />
-                </div>
-            </div>
+  return (
+    <div className="layout-container">
+      <div className="col-left">
+        <div className="title">TÀI KHOẢN CGV</div>
+        <Navbar bg="light" variant="light">
+          <Nav defaultActiveKey="#" className="flex-column">
+            <Nav.Link as={Link} to="/user">
+              THÔNG TIN CHUNG
+            </Nav.Link>
+            <Nav.Link as={Link} to="edit">
+              CHI TIẾT TÀI KHOẢN
+            </Nav.Link>
+            <Nav.Link as={Link} to="card">
+              THẺ THÀNH VIÊN
+            </Nav.Link>
+            <Nav.Link as={Link} to="point">
+              ĐIỂM THƯỞNG
+            </Nav.Link>
+            <Nav.Link as={Link} to="gift">
+              THẺ QUÀ TẶNG
+            </Nav.Link>
+            <Nav.Link as={Link} to="voucher">
+              VOUCHER
+            </Nav.Link>
+            <Nav.Link as={Link} to="coupon">
+              COUPON
+            </Nav.Link>
+            <Nav.Link as={Link} to="history">
+              LỊCH SỬ GIAO DỊCH
+            </Nav.Link>{" "}
+          </Nav>
+        </Navbar>
+      </div>
+      <div className="col-right">
+        <div className="main">
+          <Outlet />
         </div>
-    );
+      </div>
+    </div>
+  );
 });
 
 export default Layout;
--- cinema-fe/src/routes/routes.jsx
+++ cinema-fe/src/routes/routes.jsx
@@ -1,23 +1,23 @@
-import { Routes, Route } from "react-router-dom"
-import Layout from "../components/layout/default/Layout"
-import Home from "../pages/home/Home"
-import Movies from "../pages/movies/Movies"
-import MovieDescription from "../pages/movieDescription/MovieDescription"
-import ShowTimes from "../pages/showTimes/ShowTimes"
-import BookTicket from "../pages/bookTicket/BookTicket"
-import NotFound from "../pages/notFound/NotFound"
-import About from "../pages/about/About"
-import Term from "../pages/term/term"
-import TermsUse from "../pages/termsUse/TermsUse"
-import Contact from "../pages/contact/Contact"
-import UserMenu from '../pages/admin/User'
-import FilmMenu from "../pages/admin/Film"
-import PaymentPolicy from '../pages/paymentPolicy/PaymentPolicy'
-import FAQ from '../pages/faq/faq'
+import { Routes, Route } from "react-router-dom";
+import Layout from "../components/layout/default/Layout";
+import Home from "../pages/home/Home";
+import Movies from "../pages/movies/Movies";
+import MovieDescription from "../pages/movieDescription/MovieDescription";
+import ShowTimes from "../pages/showTimes/ShowTimes";
+import BookTicket from "../pages/bookTicket/BookTicket";
+import NotFound from "../pages/notFound/NotFound";
+import About from "../pages/about/About";
+import Term from "../pages/term/term";
+import TermsUse from "../pages/termsUse/TermsUse";
+import Contact from "../pages/contact/Contact";
+import UserMenu from "../pages/admin/User";
+import FilmMenu from "../pages/admin/Film";
+import PaymentPolicy from "../pages/paymentPolicy/PaymentPolicy";
+import FAQ from "../pages/faq/faq";
 
-import ListLayout from "../components/layout/ListLayout/ListLayout"
-import Login from '../pages/account/Login'
-import SignUp from '../pages/account/SignUp'
+import ListLayout from "../components/layout/ListLayout/ListLayout";
+import Login from "../pages/account/Login";
+import SignUp from "../pages/account/SignUp";
 
 import LayoutUser from "../pages/account/user/Layout";
 import User from "../pages/account/user/User";
@@ -29,77 +29,76 @@ import Coupon from "../pages/account/user/Coupon";
 import Info from "../pages/account/user/Info";
 
 // import Admin from '../pages/admin/Admin'
-import AccountLayout from '../components/layout/accountLayout/AccountLayout'
-import AdminLayout from '../components/layout/adminLayout/adminLayout'
-import AdminPrefetch from "../pages/admin/adminPrefetch"
-import PersistLogin from "../pages/account/PersistLogin"
-import UserPrefetch from "../components/userPrefetch/UserPrefetch"
-import CinemaMenu from "../pages/admin/Cinema"
-import { ROLES } from '../config/roles';
-import RequireAuth from "../pages/account/RequireAuth"
+import AccountLayout from "../components/layout/accountLayout/AccountLayout";
+import AdminLayout from "../components/layout/adminLayout/adminLayout";
+import AdminPrefetch from "../pages/admin/adminPrefetch";
+import PersistLogin from "../pages/account/PersistLogin";
+import UserPrefetch from "../components/userPrefetch/UserPrefetch";
+import CinemaMenu from "../pages/admin/Cinema";
+import { ROLES } from "../config/roles";
+import RequireAuth from "../pages/account/RequireAuth";
+import History from "../pages/account/user/History";
 const AppRoute = () => {
-    return (
-        <Routes>
-            <Route path='/admin'>
-                <Route index element={<Login />} />
-                <Route path='login' element={<Login />} />
-                {/* Protected Route */}
-                <Route element={<PersistLogin />} >
-                    <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />} >
-                        <Route element={<AdminPrefetch />}>
-                            <Route element={<AdminLayout />}>
-                                <Route path='user' element={<UserMenu />} />
-                                <Route path='film' element={<FilmMenu />} />
-                                <Route path='cinema' element={<CinemaMenu />} />
-                            </Route>
-                        </Route>
-                    </Route>
-                </Route>
-                {/* End Protected Route */}
+  return (
+    <Routes>
+      <Route path="/admin">
+        <Route index element={<Login />} />
+        <Route path="login" element={<Login />} />
+        {/* Protected Route */}
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+            <Route element={<AdminPrefetch />}>
+              <Route element={<AdminLayout />}>
+                <Route path="user" element={<UserMenu />} />
+                <Route path="film" element={<FilmMenu />} />
+                <Route path="cinema" element={<CinemaMenu />} />
+              </Route>
             </Route>
-            <Route path='/' element={<Layout />}>
-                <Route element={<UserPrefetch />}>
-                    <Route index element={<Home />} />
-                    <Route path='account' element={<AccountLayout />}>
-                        <Route index element={<Login />} />
-                        <Route path='login' element={<Login />} />
-                        <Route path='register' element={<SignUp />} />
-                    </Route>
-                    <Route path="user" element={<LayoutUser />}>
-                        <Route index element={<User />} />
-                        <Route path="point" element={<Point />} />
-                        <Route path="card" element={<Card />} />
-                        <Route path="gift" element={<Gift />} />
-                        <Route path="voucher" element={<Voucher />} />
-                        <Route path="coupon" element={<Coupon />} />
-                        <Route path="edit" element={<Info />} />
-                    </Route>
-                    <Route element={<PersistLogin publicURL={true} />}>
-                        <Route path='movies' element={<Movies />} />
-                        <Route path='show-times' element={<ShowTimes/>} />
-                        <Route path='book-ticket' element={<BookTicket/>} />
-                
+          </Route>
+        </Route>
+        {/* End Protected Route */}
+      </Route>
+      <Route path="/" element={<Layout />}>
+        <Route element={<UserPrefetch />}>
+          <Route index element={<Home />} />
+          <Route path="account" element={<AccountLayout />}>
+            <Route index element={<Login />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<SignUp />} />
+          </Route>
+          <Route path="user" element={<LayoutUser />}>
+            <Route index element={<User />} />
+            <Route path="point" element={<Point />} />
+            <Route path="card" element={<Card />} />
+            <Route path="gift" element={<Gift />} />
+            <Route path="voucher" element={<Voucher />} />
+            <Route path="coupon" element={<Coupon />} />
+            <Route path="edit" element={<Info />} />
+            <Route path="history" element={<History />} />
+          </Route>
+          <Route element={<PersistLogin publicURL={true} />}>
+            <Route path="movies" element={<Movies />} />
+            <Route path="show-times" element={<ShowTimes />} />
+            <Route path="book-ticket" element={<BookTicket />} />
 
-                        <Route path='movie-description' >
-                            <Route path=':slug' element={<MovieDescription />} />
-                        </Route>
-                        <Route path='default' element={<ListLayout />}>
-                            <Route index element={<About />} />
-                            <Route path='about' element={<About />} />
-                            <Route path='contact' element={<Contact />} />
-                            <Route path='terms-conditions' element={<Term />} />
-                            <Route path='terms-use' element={<TermsUse />} />
-                            <Route path='payment-policy' element={<PaymentPolicy />} />
-                            <Route path='faq' element={<FAQ />} />
-                        </Route>
-                        <Route path='*' element={<NotFound />} />
-                    </Route>
-                </Route>
+            <Route path="movie-description">
+              <Route path=":slug" element={<MovieDescription />} />
             </Route>
-            <Route path='*' element={<NotFound />} />
-        </Routes>
-
-
-    )
-}
-export default AppRoute
+            <Route path="default" element={<ListLayout />}>
+              <Route index element={<About />} />
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="terms-conditions" element={<Term />} />
+              <Route path="terms-use" element={<TermsUse />} />
+              <Route path="payment-policy" element={<PaymentPolicy />} />
+              <Route path="faq" element={<FAQ />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Route>
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+export default AppRoute;
